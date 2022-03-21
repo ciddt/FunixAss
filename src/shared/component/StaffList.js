e481b4bb48e68fe37dcb706fa7f2b0875394ba2d
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-03-21 19:27:48 +0700

    Install bootstrap and build basic structure of component: App and StaffList

---
+++ src/shared/component/StaffList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class StaffList extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default StaffList;
