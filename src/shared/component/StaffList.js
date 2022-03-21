4a72184195dd9f8ba1b750935941bcdc99d9dfd6
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-03-21 20:17:24 +0700

    Add props column to StaffListCard

--- src/shared/component/StaffList.js
+++ src/shared/component/StaffList.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import {} from "bootstrap/dist/css/bootstrap.min.css";
 import dateFormat from "dateformat";
 
-const StaffListCard = ({staffData}) => (
-    <div className="col-12 col-md-6 col-lg-4">
+const StaffListCard = ({staffData, column}) => (
+    <div className={column.default}>
         <div className="card mt-1 border-warning">
             <div className="card-body">
                 {staffData.name}
@@ -13,7 +13,17 @@ const StaffListCard = ({staffData}) => (
     </div>
 )
 class StaffList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            columnSeclected: {
+                default: "col-12 col-md-6 col-lg-4"
+            }
+        }
+    }
+
     render() {
+        
         return (
             <div className="container">
                 <div className="row">
@@ -22,6 +32,7 @@ class StaffList extends Component {
                             <StaffListCard
                                 key={staff.id}
                                 staffData={staff}
+                                column={this.state.columnSeclected}
                             />
                         ))
                     }
