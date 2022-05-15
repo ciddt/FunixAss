1124860e5c6dd05862ab6bf326bab79e379ca757
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-05-15 14:00:54 +0700

    Change to local host

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,2 +1,4 @@
 //Save url in baseUrl
-export const baseUrl = "https://rjs101xbackend.herokuapp.com/";
+
+export const baseUrl = 'http://localhost:8080/'
+//export const baseUrl = "https://rjs101xbackend.herokuapp.com/";
